ab52fe2ccea44f933f48526cd604b85c2716db28
xuzhida <xzd408942482@gmail.com>
2020-05-17 17:48:35 +0800

    init commit

---
+++ src/main/java/com.leetcode/bitwise/FindTheDifference.java
@@ -0,0 +1,28 @@
+package com.leetcode.bitwise;
+
+/**
+ * 位运算
+ * 异或：相同为零 不同为一
+ * LeetCode 389
+ */
+public class FindTheDifference {
+
+    private static String s = "abcd";
+    private static String t = "abcde";
+
+    public static char solve(String s, String t) {
+        char c = t.charAt(t.length() - 1);
+        for (int i = 0; i < s.length(); i++) {
+            c ^= s.charAt(i);
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solve(s, t));
+
+    }
+
+}
