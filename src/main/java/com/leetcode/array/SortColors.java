225e92b65dc3f29a13d2fa5a5b623ef7902b28cb
xuzhida <xzd408942482@gmail.com>
2020-05-27 13:56:08 +0800

    75. Sort Colors（荷兰国旗）

---
+++ src/main/java/com/leetcode/array/SortColors.java
@@ -0,0 +1,53 @@
+package com.leetcode.array;
+
+import java.util.Arrays;
+
+public class SortColors {
+
+    /**
+     * Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+     * <p>
+     * Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+     * <p>
+     * Note: You are not suppose to use the library's sort function for this problem.
+     * <p>
+     * Example:
+     * <p>
+     * Input: [2,0,2,1,1,0]
+     * Output: [0,0,1,1,2,2]
+     *
+     * @param nums
+     */
+
+    public static int[] sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        int index = 0;
+
+        while (index <= right) {
+            if (nums[index] == 0) {
+                swap(nums, index++, left++);
+            } else if (nums[index] == 1) {
+                index++;
+            } else {
+                swap(nums, index, right--);
+            }
+        }
+        return nums;
+    }
+
+    public static void swap(int[] num, int i, int j) {
+        int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 0, 2, 1, 1, 0};
+        System.out.println(Arrays.toString(sortColors(nums)));
+    }
+
+}
